3199202f28d1aea5797d65d70635efe1edaa83a8
xinwuye <1010647987@qq.com>
2025-06-02 01:40:41 -0400

    removed some unnecessary files in models.

--- train/model_interface.py
+++ train/model_interface.py
@@ -74,6 +74,54 @@ class MInterface(MInterface_base):
         loss, recovery = self(batch)
         self.log('loss', loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss
+    
+
+    def validation_step(self, batch, batch_idx):
+        loss, recovery = self(batch)
+        self.log_dict({"val_loss":loss,
+                       "recovery": recovery})
+        
+        return self.log_dict
+
+
+    def test_step(self, batch, batch_idx):
+        if not self.test_setupped:
+            self.test_setup()   
+            self.test_setupped = True 
+        # Set model to evaluation mode and disable gradients
+        self.model.eval()
+        # losses, recoveries, plddt_cas, plddts, rmsds, tmscores, nssr_scores, hydrophobicity_rmsds, charge_rmsds = self.test_forward(batch)
+        with torch.no_grad():
+            # losses, recoveries, plddt_cas, plddts, rmsds, tmscores, nssr_scores, hydrophobicity_rmsds, charge_rmsds, surface_recoveries, core_recoveries = self.test_forward(batch)
+            losses, recoveries, plddt_cas, plddts, rmsds, tmscores, nssr_scores, hydrophobicity_rmsds, charge_rmsds = self.test_forward(batch)
+
+            for i, loss in enumerate(losses):
+                recovery = recoveries[i]
+                plddt_ca = plddt_cas[i]
+                plddt = plddts[i]
+                rmsd = rmsds[i]
+                tmscore = tmscores[i]
+                nssr_score = nssr_scores[i]
+                hydrophobicity_rmsd = hydrophobicity_rmsds[i]
+                charge_rmsd = charge_rmsds[i]
+                # surface_recovery = surface_recoveries[i]
+                # core_recovery = core_recoveries[i]
+                self.test_step_outputs.append({
+                        "test_loss": loss,
+                        "test_recovery": recovery,
+                        'test_plddt_ca': plddt_ca, 
+                        'test_plddt': plddt,
+                        'test_rmsd': rmsd,
+                        'test_tmscore': tmscore,
+                        'test_nssr_score': nssr_score,
+                        'test_hydrophobicity_rmsd': hydrophobicity_rmsd,
+                        'test_charge_rmsd': charge_rmsd,
+                        # 'test_surface_recovery': surface_recovery,
+                        # 'test_core_recovery': core_recovery,
+                    }
+                )
+
+        return
         
     def load_model(self):
         params = OmegaConf.load(f'./src/models/configs/{self.hparams.model_name}.yaml')
