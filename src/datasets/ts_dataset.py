3df237c166156b91dc464b13c58a9451cecf6788
xinwuye <1010647987@qq.com>
2025-11-10 03:32:46 -0500

    clear unused files

--- src/datasets/ts_dataset.py
+++ src/datasets/ts_dataset.py
@@ -18,49 +18,6 @@ def normalize_coordinates(surface):
     return normalized_surface
 
 
-class TSDataset(data.Dataset):
-    def __init__(self, path = './', split='test'):
-        if not os.path.exists(path):
-            raise "no such file:{} !!!".format(path)
-        else:
-            ts50_data = json.load(open(path+'/ts50.json'))
-            ts500_data = json.load(open(path+'/ts500.json'))
-
-        # TS500 has proteins with lengths of 500+
-        # TS50 only contains proteins with lengths less than 500
-        self.data = []
-        for temp in ts50_data:
-            coords = np.array(temp['coords'])
-            self.data.append({'title':temp['name'],
-                                'seq':temp['seq'],
-                                'CA':coords[:,1,:],
-                                'C':coords[:,2,:],
-                                'O':coords[:,3,:],
-                                'N':coords[:,0,:],
-                                'category': 'ts50'
-                                })
-
-        for temp in ts500_data:
-            coords = np.array(temp['coords'])
-            self.data.append({'title':temp['name'],
-                                'seq':temp['seq'],
-                                'CA':coords[:,1,:],
-                                'C':coords[:,2,:],
-                                'O':coords[:,3,:],
-                                'N':coords[:,0,:],
-                                'category': 'ts500'
-                                })
-
-    def __len__(self):
-        return len(self.data)
-    
-    def get_item(self, index):
-        return self.data[index]
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-
 class TS50Dataset(data.Dataset):
     def __init__(self, path = './', split='test'):
         self.path = path
